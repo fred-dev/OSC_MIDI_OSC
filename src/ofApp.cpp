07384e8fd82ec9e3017d28622aca3161e6b286be
“fred-dev” <fredaudio@gmail.com>
2023-10-02 15:40:24 +1100

    Input setting and prepare for font

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -10,6 +10,7 @@ void ofApp::setup() {
     ofBackground(255);
     
     activityMessage = "No messages received";
+    setupTextInputFields();
 
     
 }
@@ -26,57 +27,34 @@ void ofApp::draw() {
     //operationmode switch
     switch (operationMode) {
         case MODE_SETTING_MIDI_IN:
-            //dra the list of midi in ports and highlight the selcted one
-            ofDrawBitmapString("Midi in settings", 10, 15);
-
-            for (int i = 0; i < appSettings["allInPorts"].size(); i++) {
-                ofPushStyle();
-                if (appSettings["allInPorts"][i] == appSettings["inPortLabel"]) {
-                    ofSetColor(255, 0, 0);
-                } else {
-                    ofSetColor(0);
-                }
-                ofDrawBitmapString(appSettings["allInPorts"][i], 10, 30 + (i * 15));
-                ofPopStyle();
-            }
-            
-            //draw the midi in channel to right of these messages
-            ofSetColor(0);
-            ofDrawBitmapString("Midi channel set to: " + ofToString(appSettings["midiInChannel"]), 320, 30);
-            
-            
-            //MODE_SETTING_MID_IN
+            drawMidiSettings("Midi in settings", appSettings["allInPorts"], appSettings["inPortLabel"], appSettings["midiInChannel"]);
             break;
+            
         case MODE_SETTING_MIDI_OUT:
-            ofDrawBitmapString("Midi out settings", 10, 15);
-
-            for (int i = 0; i < appSettings["allOutPorts"].size(); i++) {
-                ofPushStyle();
-                if (appSettings["allOutPorts"][i] == appSettings["outPortLabel"]) {
-                    ofSetColor(255, 0, 0);
-                } else {
-                    ofSetColor(0);
-                }
-                ofDrawBitmapString(appSettings["allOutPorts"][i], 10, 30 + (i * 15));
-                ofPopStyle();
-            }
-            
-            //draw the midi in channel to right of these messages
-            ofSetColor(0);
-            ofDrawBitmapString("Midi channel set to: " + ofToString(appSettings["midiOutChannel"]), 320, 30);
-            //MODE_SETTING_MIDI_OUT
+            drawMidiSettings("Midi out settings", appSettings["allOutPorts"], appSettings["outPortLabel"], appSettings["midiOutChannel"]);
             break;
+            
         case MODE_SETTING_OSC_IN:
             ofDrawBitmapString("OSC in settings", 10, 15);
+            ofSetColor(0);
+            ofRectangle(oscInPortField.bounds);
+            ofNoFill();
+            oscInPortField.draw();
+            
             //draw the osc in port
-            ofDrawBitmapString("Osc in port: " + ofToString(appSettings["incomingPortOsc"]), 10, 30);
+            //ofDrawBitmapString("Osc in port: " + ofToString(appSettings["incomingPortOsc"]), 10, 30);
 
             //MODE_SETTING_OSC_IN
             break;
         case MODE_SETTING_OSC_OUT:
-            ofDrawBitmapString("OSC Out settings", 10, 15);
-            ofDrawBitmapString("Osc in port: " + ofToString(appSettings["outGoingPortOsc"]), 10, 30);
-            ofDrawBitmapString("Osc in port: " + ofToString(appSettings["outgoingIpOSC"]), 10, 45);
+            ofDrawBitmapString("OSC out settings", 10, 15);
+            ofSetColor(0);
+            ofRectangle(oscOutPortField.bounds);
+            ofNoFill();
+            oscOutPortField.draw();
+            ofRectangle(oscOutIPField.bounds);
+            ofNoFill();
+            oscOutIPField.draw();
 
             //MODE_SETTING_OSC_OUT
             break;
@@ -99,18 +77,28 @@ void ofApp::keyPressed(int key) {
     
     switch (key) {
         case '1':
+            disableAllTextInputFields();
             operationMode = MODE_SETTING_MIDI_IN;
             break;
         case '2':
+            disableAllTextInputFields();
             operationMode = MODE_SETTING_MIDI_OUT;
             break;
         case '3':
+            disableAllTextInputFields();
             operationMode = MODE_SETTING_OSC_IN;
+            oscInPortField.enable();
+            
             break;
         case '4':
+            disableAllTextInputFields();
             operationMode = MODE_SETTING_OSC_OUT;
+            oscOutPortField.enable();
+            oscOutIPField.enable();
+            
             break;
         case '5':
+            disableAllTextInputFields();
             operationMode = MODE_CONVERSION;
             break;
     }
@@ -118,84 +106,17 @@ void ofApp::keyPressed(int key) {
     //operationmode switch
     switch (operationMode) {
         case MODE_SETTING_MIDI_IN:
-            //MODE_SETTING_MID_IN
-            //use the up and down keys to select the midi in port
-            //first use the appSettings["inPortLabel"] to check whci of the ports are used in appSettings["allInPorts"] then incremener them as needed
+            if (key == OF_KEY_DOWN) selectPort(appSettings["allInPorts"], appSettings["inPortLabel"], 1);
+            if (key == OF_KEY_UP) selectPort(appSettings["allInPorts"], appSettings["inPortLabel"], -1);
+            if (key == OF_KEY_LEFT) selectChannel(appSettings["midiInChannel"], -1);
+            if (key == OF_KEY_RIGHT) selectChannel(appSettings["midiInChannel"], 1);
             
-            if (key == OF_KEY_DOWN) {
-                //increment the selected port
-                for (int i = 0; i < appSettings["allInPorts"].size(); i++) {
-                    if (appSettings["allInPorts"][i] == appSettings["inPortLabel"]) {
-                        if (i < appSettings["allInPorts"].size() - 1) {
-                            appSettings["inPortLabel"] = appSettings["allInPorts"][i + 1];
-                            break;
-                        }
-                    }
-                }
-            }
-            if (key == OF_KEY_UP) {
-                //decrement the selected port
-                for (int i = 0; i < appSettings["allInPorts"].size(); i++) {
-                    if (appSettings["allInPorts"][i] == appSettings["inPortLabel"]) {
-                        if (i > 0) {
-                            appSettings["inPortLabel"] = appSettings["allInPorts"][i - 1];
-                            break;
-                        }
-                    }
-                }
-            }
-            //use the left and right arrows to slect the midi in channel, get it from the settings first appSettings["midiInChannel"] it can only be 1 - 16
-            if (key == OF_KEY_LEFT) {
-                if (appSettings["midiInChannel"] > 1) {
-                    appSettings["midiInChannel"] = appSettings["midiInChannel"].get<int>() - 1;
-                }
-            }
-            
-            if (key == OF_KEY_RIGHT) {
-                if (appSettings["midiInChannel"] < 16) {
-                    appSettings["midiInChannel"] = appSettings["midiInChannel"].get<int>() + 1;
-                }
-            }
-            
-          
             break;
         case MODE_SETTING_MIDI_OUT:
-            //MODE_SETTING_MIDI_OUT
-            
-            if (key == OF_KEY_DOWN) {
-                //increment the selected port
-                for (int i = 0; i < appSettings["allOutPorts"].size(); i++) {
-                    if (appSettings["allOutPorts"][i] == appSettings["outPortLabel"]) {
-                        if (i < appSettings["allOutPorts"].size() - 1) {
-                            appSettings["outPortLabel"] = appSettings["allOutPorts"][i + 1];
-                            break;
-                        }
-                    }
-                }
-            }
-            if (key == OF_KEY_UP) {
-                //decrement the selected port
-                for (int i = 0; i < appSettings["allOutPorts"].size(); i++) {
-                    if (appSettings["allOutPorts"][i] == appSettings["outPortLabel"]) {
-                        if (i > 0) {
-                            appSettings["outPortLabel"] = appSettings["allOutPorts"][i - 1];
-                            break;
-                        }
-                    }
-                }
-            }
-            //use the left and right arrows to slect the midi in channel, get it from the settings first appSettings["midiInChannel"] it can only be 1 - 16
-            if (key == OF_KEY_LEFT) {
-                if (appSettings["midiOutChannel"] > 1) {
-                    appSettings["midiOutChannel"] = appSettings["midiOutChannel"].get<int>() - 1;
-                }
-            }
-            
-            if (key == OF_KEY_RIGHT) {
-                if (appSettings["midiOutChannel"] < 16) {
-                    appSettings["midiOutChannel"] = appSettings["midiOutChannel"].get<int>() + 1;
-                }
-            }
+            if (key == OF_KEY_DOWN) selectPort(appSettings["allOutPorts"], appSettings["outPortLabel"], 1);
+            if (key == OF_KEY_UP) selectPort(appSettings["allOutPorts"], appSettings["outPortLabel"], -1);
+            if (key == OF_KEY_LEFT) selectChannel(appSettings["midiOutChannel"], -1);
+            if (key == OF_KEY_RIGHT) selectChannel(appSettings["midiOutChannel"], 1);
             break;
         case MODE_SETTING_OSC_IN:
             //MODE_SETTING_OSC_IN
@@ -241,3 +162,101 @@ void ofApp::gotMessage(ofMessage msg) {
 void ofApp::dragEvent(ofDragInfo dragInfo) {
 }
 
+// Helper function to handle port selection
+void ofApp::selectPort(ofJson& ports, ofJson& currentPort, int direction) {
+    for (int i = 0; i < ports.size(); i++) {
+        if (ports[i] == currentPort) {
+            int newIndex = ofClamp(i + direction, 0, ports.size() - 1);
+            currentPort = ports[newIndex];
+            break;
+        }
+    }
+}
+
+// Helper function to handle channel selection
+void ofApp::selectChannel(ofJson& channel, int direction) {
+    int newChannel = ofClamp(channel.get<int>() + direction, 1, MAX_MIDI_CHANNEL);
+    channel = newChannel;
+}
+
+void ofApp::drawMidiSettings(const std::string& title, const ofJson& ports, const ofJson& currentPort, int channel) {
+    // Draw the title
+    ofDrawBitmapString(title, 10, 15);
+
+    // Draw the list of ports and highlight the selected one
+    for (int i = 0; i < ports.size(); i++) {
+        ofPushStyle();
+        if (ports[i] == currentPort) {
+            ofSetColor(255, 0, 0);
+        } else {
+            ofSetColor(0);
+        }
+        ofDrawBitmapString(ports[i], 10, 30 + (i * 15));
+        ofPopStyle();
+    }
+
+    // Draw the midi channel to the right of these messages
+    ofSetColor(0);
+    ofDrawBitmapString("Midi channel set to: " + ofToString(channel), 320, 30);
+}
+
+bool ofApp::isValidIPAddress(std::string ip){
+    //check if the string is a alid IP address
+//    struct sockaddr_in sa;
+//    int result = inet_pton(AF_INET, ip.c_str(), &(sa.sin_addr));
+//    return result != 0;
+    
+    return false;
+}
+
+void ofApp::setupTextInputFields(){
+
+    oscInPortField.setup();
+    oscInPortField.text = ofToString(appSettings["incomingPortOsc"]);
+    oscInPortField.bounds.x = 10;
+    oscInPortField.bounds.y = 30;
+    oscInPortField.bounds.height = 15;
+    oscInPortField.bounds.width = 188;
+    ofAddListener(oscInPortField.onTextChange, this, &ofApp::onOSCInputPortEditied);
+
+    
+    oscOutPortField.setup();
+    oscOutPortField.text = ofToString(appSettings["outGoingPortOsc"]);
+    oscOutPortField.bounds.x = 10;
+    oscOutPortField.bounds.y = 30;
+    oscOutPortField.bounds.height = 15;
+    oscOutPortField.bounds.width = 188;
+    ofAddListener(oscOutPortField.onTextChange, this, &ofApp::onOSCOutputPortEditied);
+
+    
+    oscOutIPField.setup();
+    oscOutIPField.text = appSettings["outgoingIpOSC"];
+    oscOutIPField.bounds.x = 10;
+    oscOutIPField.bounds.y = 45;
+    oscOutIPField.bounds.height = 15;
+    oscOutIPField.bounds.width = 188;
+    ofAddListener(oscOutIPField.onTextChange, this, &ofApp::onOSCOutputIPEditied);
+
+}
+
+void ofApp::disableAllTextInputFields(){
+    oscInPortField.disable();
+    oscOutPortField.disable();
+    oscOutIPField.disable();
+}
+
+void ofApp::onOSCInputPortEditied( string& message){
+    
+    ofLogNotice() << "onOSCInputPortEditied: " << message;
+    
+    
+}
+void ofApp::onOSCOutputPortEditied(string& message){
+        
+        ofLogNotice() << "onOSCOutputPortEditied: " << message;
+}
+void ofApp::onOSCOutputIPEditied(string& message){
+    
+    ofLogNotice() << "onOSCOutputIPEditied: " << message;
+    
+}
--- src/ofApp.h
+++ src/ofApp.h
@@ -4,13 +4,18 @@
 #include "SettingsManager.h"
 #include "MidiManager.h"
 #include "OscManager.h"
+#include "ofxTextInputField.h"
 
-#define MODE_SETTING_MIDI_IN 0
-#define MODE_SETTING_MIDI_OUT 1
-#define MODE_SETTING_OSC_IN 2
-#define MODE_SETTING_OSC_OUT 3
-#define MODE_CONVERSION 4
 
+enum OperationMode {
+    MODE_SETTING_MIDI_IN,
+    MODE_SETTING_MIDI_OUT,
+    MODE_SETTING_OSC_IN,
+    MODE_SETTING_OSC_OUT,
+    MODE_CONVERSION
+};
+
+const int MAX_MIDI_CHANNEL = 16;
 
 class ofApp : public ofBaseApp {
 
@@ -42,6 +47,25 @@ class ofApp : public ofBaseApp {
     std::vector<std::string> midiOutPortList;
     int selectedInPort, selectedOutPort;
 
+    // Helper function to handle port selection
+    void selectPort(ofJson& ports, ofJson& currentPort, int direction);
 
-		
+    // Helper function to handle channel selection
+    void selectChannel(ofJson& channel, int direction);
+    
+    void drawMidiSettings(const std::string& title, const ofJson& ports, const ofJson& currentPort, int channel);
+    
+    bool isValidIPAddress(std::string ip);
+    
+    ofxTextInputField oscInPortField, oscOutPortField, oscOutIPField;
+    void setupTextInputFields();
+    
+    void disableAllTextInputFields();
+    
+    void onOSCInputPortEditied(string& message);
+    void onOSCOutputPortEditied(string& message);
+    void onOSCOutputIPEditied(string& message);
+    
+    
+
 };
