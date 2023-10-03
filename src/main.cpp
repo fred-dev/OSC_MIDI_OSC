5024bb752e196e2b4e727e038dc46af390c5716b
Frederick Rodrigues <fredaudio@gmail.com>
2023-10-04 07:59:03 +1100

    fixed windows icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
+
 #include "ofMain.h"
 #include "ofApp.h"
+
+
+#ifdef TARGET_WIN32
+
+#define IDI_ICON1 101
+
+#endif
 
 //========================================================================
 int main( ){
@@ -11,6 +19,13 @@ int main( ){
 
     auto window = ofCreateWindow(settings);
 
+#ifdef TARGET_WIN32
+    HWND hwnd = ofGetWin32Window();
+	// Load and set the application icon
+	HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+	SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon); // Set the large icon
+	SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon); // Set the small icon
+#endif
     ofRunApp(window, std::make_shared<ofApp>());
     ofRunMainLoop();
 
